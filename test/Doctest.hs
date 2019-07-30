42886ad7fa5d9a4053d3c611038711b9f590af00
vrom911 <vrom911@gmail.com>
2019-07-30 18:09:45 +0800

    Do not run doctest on old GHC

--- test/Doctest.hs
+++ test/Doctest.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE CPP #-}
+
 module Main (main) where
 
 import System.FilePath.Glob (glob)
 import Test.DocTest (doctest)
 
 main :: IO ()
+#if ( __GLASGOW_HASKELL__ >= 804 )
 main = do
     sourceFiles <- glob "src/**/*.hs"
     doctest
@@ -11,3 +14,6 @@ main = do
         : "-XOverloadedStrings"
         : "-XScopedTypeVariables"
         : sourceFiles
+#else
+main = pure ()
+#endif
